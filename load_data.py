4fa458549544610d2200fb541b766e7426016e7c
cmehta <chetanmehta0204@gmail.com>
2021-10-17 09:46:45 +0530

    Added S3 reading logic

--- load_data.py
+++ load_data.py
@@ -1,10 +1,18 @@
 import pandas as pd
+import boto3
 
 
-def read_csv(file_name, headerList):
-    file = pd.read_csv(file_name)
-    file.to_csv(file_name, header=headerList, index=False)
-    file_df = pd.read_csv(file_name)
+def read_4m_s3(AWS_S3_BUCKET, file):
+    s3_client = boto3.client('s3')
+    response = s3_client.get_object(Bucket=AWS_S3_BUCKET, Key=file)
+    status = response.get("ResponseMetadata", {}).get("HTTPStatusCode")
+    return response, status
+
+
+def read_csv(file_obj, headerList):
+    file = pd.read_csv(file_obj)
+    file.to_csv(file_obj, header=headerList, index=False)
+    file_df = pd.read_csv(file_obj)
     return file_df
 
 
@@ -13,11 +21,35 @@ def load_2_dynamo():
 
 
 if __name__ == '__main__':
-    artist_summary_headerList = ['ArtistSummaryId', 'RoySys', 'Acct_No', 'Acct_Qtr', 'Seq_no', 'Payee_No', 'Owner_name', 'Account_Name', 'Vendor_No', 'Acct_Status', 'Acct_Payee_Status', 'Payee_Status', 'Opening_Bal', 'Prior_Resv', 'Total_Resv', 'Total_Payments', 'Total_Adjustments', 'Dom_Earnings', 'Club_Earnings', '3rd_Party_Earnings', 'Foreign_Earnings', 'Total_Earn', 'Total_Transfers', 'Ending_Bal', 'PCD', 'A_Prior_Resv', 'A_Total_Resv', 'A_Dom_Earnings', 'A_Club_Earnings', 'A_3rd_Party_Earnings', 'A_Foreign_Earnings', 'Payee_Name', 'Address_1', 'Address_2', 'Address_3', 'Address_4', 'Payee_Pct', 'A_Total_Earn', 'Active', 'ASL', 'NonAccrued', 'Total_MiscEarnings', 'A_Total_MiscEarnings', 'IsArchived']
-    artist_summary_df = read_csv('artist_summary.csv', artist_summary_headerList)
+    AWS_S3_BUCKET='database-poc-extracts-east1'
+    artist_summary_headerList = ['ArtistSummaryId', 'RoySys', 'Acct_No', 'Acct_Qtr', 'Seq_no', 'Payee_No', 'Owner_name',
+                                 'Account_Name', 'Vendor_No', 'Acct_Status', 'Acct_Payee_Status', 'Payee_Status',
+                                 'Opening_Bal', 'Prior_Resv', 'Total_Resv', 'Total_Payments', 'Total_Adjustments',
+                                 'Dom_Earnings', 'Club_Earnings', '3rd_Party_Earnings', 'Foreign_Earnings',
+                                 'Total_Earn', 'Total_Transfers', 'Ending_Bal', 'PCD', 'A_Prior_Resv', 'A_Total_Resv',
+                                 'A_Dom_Earnings', 'A_Club_Earnings', 'A_3rd_Party_Earnings', 'A_Foreign_Earnings',
+                                 'Payee_Name', 'Address_1', 'Address_2', 'Address_3', 'Address_4', 'Payee_Pct',
+                                 'A_Total_Earn', 'Active', 'ASL', 'NonAccrued', 'Total_MiscEarnings',
+                                 'A_Total_MiscEarnings', 'IsArchived']
+    artist_details_headerList = ['ArtistDetailId', 'RoySys', 'Acct_No', 'Acct_Qtr', 'Seq_No', 'Payee_No', 'Vendor_No',
+                                 'Group_No', 'Source', 'Title', 'Sales Type', 'Price Level', 'Sales Date', 'Selection',
+                                 'Config', 'Contract', 'Pr_Code', 'Price', 'Pckg_rate', 'Roy_Rate', 'Part %',
+                                 'Eff_rate', 'Tax_rate', 'Net_roy_earn', 'Active', 'ASL', 'DSP Name', 'Units',
+                                 'Receipts']
 
-    artist_details_headerList = ['ArtistDetailId', 'RoySys', 'Acct_No', 'Acct_Qtr', 'Seq_No', 'Payee_No', 'Vendor_No', 'Group_No', 'Source', 'Title', 'Sales Type', 'Price Level', 'Sales Date', 'Selection', 'Config', 'Contract', 'Pr_Code', 'Price', 'Pckg_rate', 'Roy_Rate', 'Part %', 'Eff_rate', 'Tax_rate', 'Net_roy_earn', 'Active', 'ASL', 'DSP Name', 'Units', 'Receipts']
-    artist_details_df = read_csv('artist_details.csv', artist_details_headerList)
+    artist_summary_obj, status = read_4m_s3(AWS_S3_BUCKET, 'artist_summary.csv')
+    if status == 200:
+        print("Successful S3 get_object response for artist summary. Status - {}".format(status))
+        artist_summary_df = read_csv(artist_summary_obj.get("Body"), artist_summary_headerList)
+    else:
+        print("Unsuccessful S3 get_object response for artist summary. Status - {}".format(status))
+
+    artist_details_obj, status = read_4m_s3(AWS_S3_BUCKET, 'artist_details.csv')
+    if status == 200:
+        print("Successful S3 get_object response for artist details. Status - {}".format(status))
+        artist_details_df = read_csv(artist_details_obj.get("Body"), artist_details_headerList)
+    else:
+        print("Unsuccessful S3 get_object response for artist details. Status - {}".format(status))
 
     artist_summary_df['PK'] = artist_summary_df.RoySys.astype(str) + artist_summary_df.Acct_No.astype(
         str) + artist_summary_df.Acct_Qtr.astype(str) + artist_summary_df.Payee_No.astype(str)
@@ -25,4 +57,4 @@ if __name__ == '__main__':
         str) + artist_details_df.Acct_Qtr.astype(str) + artist_details_df.Payee_No.astype(str)
 
     artist_master_df = pd.merge(artist_summary_df, artist_details_df, how='inner', on='PK')
-    print(artist_master_df)
+    print(artist_master_df.head())
