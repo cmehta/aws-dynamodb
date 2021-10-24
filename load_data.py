fc9d5b6f4619964243afbf413ca30f5d13d85ee0
cmehta <chetanmehta0204@gmail.com>
2021-10-24 18:43:24 +0530

    Implemneted strategy 5.

--- load_data.py
+++ load_data.py
@@ -64,6 +64,16 @@ def fix_float_2_decimal(df):
             print('Types fixed for {}'.format(i))
 
 
+def add_primarykey(artist_master_df):
+    artist_master_df['pk'] = artist_master_df['roysys'] + artist_master_df['vendor_no'] + artist_master_df['acct_no'] +\
+                             artist_master_df['acct_qtr'] +artist_master_df['dsp_name'] +artist_master_df['title'] + \
+                             artist_master_df['source'] +artist_master_df['sales_type']
+    artist_master_df['sk'] = artist_master_df['roysys'] + artist_master_df['vendor_no'] + artist_master_df['acct_no']+ \
+                             artist_master_df['acct_qtr'] + artist_master_df['dsp_name'] + artist_master_df['title'] + \
+                             artist_master_df['source'] + artist_master_df['sales_type'] + \
+                             artist_master_df['artistdetailid']
+
+
 def batch_load_dynamo(dataframe):
     dynamodb = boto3.resource('dynamodb', region_name=REGION_NAME)
     table = dynamodb.Table(DYNAMO_TABLE)
@@ -72,14 +82,12 @@ def batch_load_dynamo(dataframe):
     try:
         with table.batch_writer() as batch:
             for index, row in dataframe.iterrows():
-                print(row)
-                print('----------------------------------')
                 if index%1000==0:
                     # logger.info("written index - %d.", index)
                     print("written index - {}.".format(index))
-                content = {'artistsummaryid': row['artistsummaryid'], 'roysys': row['roysys_x'],
-                           'acct_no': row['acct_no_x'], 'acct_qtr_x': row['acct_qtr_x'], 'seq_no': row['seq_no_x'],
-                           'payee_no': row['payee_no_x'], 'owner_name': row['owner_name'],
+                content = {'pk': row['pk'], 'sk': row['sk'], 'artistsummaryid': row['artistsummaryid'],
+                           'roysys': row['roysys_x'], 'acct_no': row['acct_no_x'], 'acct_qtr': row['acct_qtr_x'],
+                           'seq_no': row['seq_no_x'], 'payee_no': row['payee_no_x'], 'owner_name': row['owner_name'],
                            'account_name': row['account_name'], 'vendor_no': row['vendor_no_x'],
                            'acct_status': row['acct_status'], 'acct_payee_status': row['acct_payee_status'],
                            'payee_status': row['payee_status'], 'opening_bal': row['opening_bal'],
@@ -95,19 +103,17 @@ def batch_load_dynamo(dataframe):
                            'a_foreign_earnings': row['a_foreign_earnings'], 'payee_name': row['payee_name'],
                            'address_1': row['address_1'], 'address_2': row['address_2'], 'address_3': row['address_3'],
                            'address_4': row['address_4'], 'payee_pct': row['payee_pct'],
-                           'a_total_earn': row['a_total_earn'], 'active_x': row['active_x'], 'asl_x': row['asl_x'],
+                           'a_total_earn': row['a_total_earn'], 'active': row['active_x'], 'asl': row['asl_x'],
                            'nonaccrued': row['nonaccrued'], 'total_miscearnings': row['total_miscearnings'],
                            'a_total_miscearnings': row['a_total_miscearnings'], 'isarchived': row['isarchived'],
-                           'pk': row['pk'], 'artistdetailid': row['artistdetailid'], 'group_no': row['group_no'],
-                           'source': row['source'], 'title': row['title'], 'sales type': row['sales type'],
-                           'price level': row['price level'], 'sales date': row['sales date'],
+                           'artistdetailid': row['artistdetailid'], 'group_no': row['group_no'],
+                           'source': row['source'], 'title': row['title'], 'sales_type': row['sales_type'],
+                           'price_level': row['price_level'], 'sales_date': row['sales_date'],
                            'selection': row['selection'], 'config': row['config'], 'contract': row['contract'],
                            'pr_code': row['pr_code'], 'price': row['price'], 'pckg_rate': row['pckg_rate'],
-                           'roy_rate': row['roy_rate'], 'part %': row['part %'], 'eff_rate': row['eff_rate'],
+                           'roy_rate': row['roy_rate'], 'part_pct': row['part_pct'], 'eff_rate': row['eff_rate'],
                            'tax_rate': row['tax_rate'], 'net_roy_earn': row['net_roy_earn'],
-                           'dsp name': row['dsp name'], 'units': row['units'], 'receipts': row['receipts']}
-
-                print(content)
+                           'dsp_name': row['dsp_name'], 'units': row['units'], 'receipts': row['receipts']}
 
                 # Since Object of type Decimal is not JSON serializable, we are using below code to fix this issue.
                 # dict_encoded_as_json_string = json.dumps(content, cls=DecimalEncoder)
@@ -132,9 +138,9 @@ if __name__ == '__main__':
                                  'a_total_earn', 'active', 'asl', 'nonaccrued', 'total_miscearnings',
                                  'a_total_miscearnings', 'isarchived']
     artist_details_headerList = ['artistdetailid', 'roysys', 'acct_no', 'acct_qtr', 'seq_no', 'payee_no', 'vendor_no',
-                                 'group_no', 'source', 'title', 'sales type', 'price level', 'sales date', 'selection',
-                                 'config', 'contract', 'pr_code', 'price', 'pckg_rate', 'roy_rate', 'part %',
-                                 'eff_rate', 'tax_rate', 'net_roy_earn', 'active', 'asl', 'dsp name', 'units',
+                                 'group_no', 'source', 'title', 'sales_type', 'price_level', 'sales_date', 'selection',
+                                 'config', 'contract', 'pr_code', 'price', 'pckg_rate', 'roy_rate', 'part_pct',
+                                 'eff_rate', 'tax_rate', 'net_roy_earn', 'active', 'asl', 'dsp_name', 'units',
                                  'receipts']
 
     artist_summary_obj, status = read_4m_s3('artist_summary.csv')
@@ -161,12 +167,14 @@ if __name__ == '__main__':
         # logger.info("Unsuccessful S3 get_object response for artist details. Status - %s.", status)
         print("Unsuccessful S3 get_object response for artist details. Status - {}.".format(status))
 
-    artist_summary_df['pk'] = artist_summary_df.roysys + artist_summary_df.acct_no + artist_summary_df.acct_qtr\
+    artist_summary_df['joinkey'] = artist_summary_df.roysys + artist_summary_df.acct_no + artist_summary_df.acct_qtr\
                               + artist_summary_df.payee_no
-    artist_details_df['pk'] = artist_details_df.roysys + artist_details_df.acct_no + artist_details_df.acct_qtr\
+    artist_details_df['joinkey'] = artist_details_df.roysys + artist_details_df.acct_no + artist_details_df.acct_qtr \
                               + artist_details_df.payee_no
 
-    artist_master_df = pd.merge(artist_summary_df, artist_details_df, how='inner', on='pk')
+    artist_master_df = pd.merge(artist_summary_df, artist_details_df, how='inner', on='joinkey')
+
+    add_primarykey(artist_master_df)
     print(artist_master_df.head())
     print(artist_master_df.dtypes)
 
